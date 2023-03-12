856747a13d24508e3265fa0fc78975a50d2809cb
bhargav3233 <109935418+bhargav3233@users.noreply.github.com>
2023-03-13 05:29:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Mar  21 02:06:13 2022
-
-@author: Bhargav
-"""
-
 
 
 import streamlit as st
@@ -13,53 +6,52 @@ import string
 from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
-ps= PorterStemmer()
+
+ps = PorterStemmer()
 
 def text_transform(text):
-    text=text.lower()
-    text=nltk.word_tokenize(text)
+    text = text.lower()
+    text = nltk.word_tokenize(text)
     
-    #removing special chars
-    y=[]
+    # Removing special chars
+    y = []
     for i in text:
         if i.isalnum():
             y.append(i)
-    text=y[:]
+    text = y[:]
     y.clear()
     
     for i in text:
         if i not in stopwords.words('english') and i not in string.punctuation:
             y.append(i)
-            
-            
-    text=y[:]
+    
+    text = y[:]
     y.clear()
     
     for i in text:
         y.append(ps.stem(i))
     
-            
     return " ".join(y)
-
-
 
 tfidf = pickle.load(open('vectorizer.pkl','rb'))
 model  = pickle.load(open('model.pkl','rb'))
 
-st.title('Text Spam / ham Classifier')
+st.title('Text Spam / Ham Classifier')
 
-input_sms = st.text_area('Enter the text')
+input_sms = st.text_input('Enter the text')
+words = input_sms.split()
+word_count = len(words)
+
+st.write(f'Word Count: {word_count}')
 
 if st.button('Predict'):
     transformed_sms = text_transform(input_sms)
-    
+
     vector_input = tfidf.transform([transformed_sms])
-    
+
     result = model.predict(vector_input)[0]
-    
+
     if result == 1:
         st.header('Spam Detected')
     else:
         st.header('No Spam Detected')
-
-
